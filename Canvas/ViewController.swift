12925256fe7869d0f10a4698da87ba0785482734
Will Gilman <will@samadhi.tech>
2017-04-19 20:19:45 -0700

    Milestones completed.

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    @IBOutlet weak var deadImageView: UIImageView!
+    @IBOutlet weak var excitedImageView: UIImageView!
+    @IBOutlet weak var happyImageView: UIImageView!
+    @IBOutlet weak var sadImageView: UIImageView!
+    @IBOutlet weak var tongueImageView: UIImageView!
+    @IBOutlet weak var winkImageView: UIImageView!
+    @IBOutlet weak var downArrowImageView: UIImageView!
+    
+    var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        trayCenterWhenOpen = trayView.center
+        trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: trayView.center.y + 210.0)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onPanGesture(_ sender: UIPanGestureRecognizer) {
+        
+        let location = sender.location(in: view)
+        // let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            
+            print("Gesture began at: \(location)")
+            trayOriginalCenter = trayView.center
+            
+        } else if sender.state == .changed {
+            
+            print("Gesture changed at: \(location)")
+            
+            let velocity = sender.velocity(in: trayView)
+            
+            if velocity.y < 0 {
+                
+                trayView.center = trayCenterWhenOpen
+            
+            } else {
+                
+//                trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+                trayView.center = trayCenterWhenClosed
+            }
+            
+        } else if sender.state == .ended {
+            
+            print("Gesture ended at: \(location)")
+        }
     }
-
 
 }
 
